6cd27f6f3be6f40ca8e419cecdedc5c4656c0461
rahosung <110014839+rahosung@users.noreply.github.com>
2023-04-03 13:39:07 +0900

    Create component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
-
+//사용자 정의 태그 = 컴포넌트
+function Header(){
+  return <header>
+    <h1><a href='/'>React</a></h1>
+  </header>
+}
+function Nav(){
+  return <nav>
+    <ol>
+      <li><a href='/read/1'>html</a></li>
+      <li><a href='/read/1'>css</a></li>
+      <li><a href='/read/1'>js</a></li>
+    </ol>
+  </nav>
+}
+function Article(){
+  return <article>
+    <h2>Welcome</h2>
+    Hello, WEB
+  </article>
+}
 function App() {
   return (
-    <div className="App">
-      Hello React!
+    <div>
+      <Header></Header>
+      <Nav></Nav>
+      <Article></Article>
     </div>
   );
 }
