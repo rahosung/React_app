7c71a6cee51f3151e8b93b4f65cf1823708242a3
rahosung <110014839+rahosung@users.noreply.github.com>
2023-04-12 18:53:28 +0900

    React event function

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-function Header(props){
+
+function Header(props) {
   console.log('props', props, props.title);
   return <header>
-    <h1><a href="/">{props.title}</a></h1>
+    <h1><a href="/" onClick={(event) => {
+      event.preventDefault();
+      props.onChangeMode();
+    }}>{props.title}</a></h1>
   </header>
 }
-function Nav(props){
+
+function Nav(props) {
   const lis = []
-  for(let i=0; i<props.topics.length; i++){
+  for (let i = 0; i < props.topics.length; i++) {
     let t = props.topics[i];
-    lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)
+    lis.push(<li key={t.id}>
+      <a id={t.id} href={'/read/' + t.id} onClick={event => {
+        event.preventDefault();
+        props.onChangeMode(event.target.id);
+      }}>{t.title}</a>
+    </li>)
   }
   return <nav>
     <ol>
@@ -18,25 +28,33 @@ function Nav(props){
     </ol>
   </nav>
 }
-function Article(props){
+
+function Article(props) {
   return <article>
     <h2>{props.title}</h2>
     {props.body}
   </article>
 }
+
 function App() {
   const topics = [
-    {id:1, title:'html', body:'html is ...'},
-    {id:2, title:'css', body:'css is ...'},
-    {id:3, title:'javascript', body:'javascript is ...'}
-  ] 
+    { id: 1, title: 'html', body: 'html is ...' },
+    { id: 2, title: 'css', body: 'css is ...' },
+    { id: 3, title: 'javascript', body: 'javascript is ...' }
+  ]
   return (
     <div>
-      <Header title="WEB"></Header>
-      <Nav topics={topics}></Nav>
+      <Header title="WEB" onChangeMode={() => {
+        alert('Header');
+      }}></Header>
+
+      <Nav topics={topics} onChangeMode={(id) => {
+        alert(id);
+      }}></Nav>
+
       <Article title="Welcome" body="Hello, WEB"></Article>
     </div>
   );
 }
- 
+
 export default App;
